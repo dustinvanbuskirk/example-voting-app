f2cd009fb5c3abfbf3be2e3fdce1247da27082da
Dustin Van Buskirk <dev@vanbuskirk.me>
2018-10-05 15:41:48 -0700

    adding commit

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -32,7 +32,7 @@ def browser():
 
 def test_confirm_vote_title(browser):
     browser.get("http://{}:80".format(vote_endpoint_ip))
-    assert "AKS vs Nodes!" in browser.title
+    assert "Codefresh vs VSTS!" in browser.title
 
 
 def test_confirm_vote_choice_form(browser):

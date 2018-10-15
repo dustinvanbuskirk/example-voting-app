6ea36830d52ef82bce019d0e49f5b2904e3c4a80
Dustin Van Buskirk <dev@vanbuskirk.me>
2018-10-15 09:51:21 -0700

    committing

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Monday")
-option_b = os.getenv('OPTION_B', "Friday")
+option_a = os.getenv('OPTION_A', "Garfield")
+option_b = os.getenv('OPTION_B', "Heathcliff")
 hostname = socket.gethostname()
 
 app = Flask(__name__)

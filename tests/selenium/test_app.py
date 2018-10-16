dc4f3f78199773673583e473bdea7c0fc5b08752
Dustin Van Buskirk <dev@vanbuskirk.me>
2018-10-16 09:31:04 -0700

    making demo

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -32,7 +32,7 @@ def browser():
 
 def test_confirm_vote_title(browser):
     browser.get("http://{}:80".format(vote_endpoint_ip))
-    assert "Garfield vs Heathcliff!" in browser.title
+    assert "BitBucket vs GitHub!" in browser.title
 
 
 def test_confirm_vote_choice_form(browser):

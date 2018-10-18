ea2534fb751c9a47070447daec1be5891f1700d3
Dustin Van Buskirk <dev@vanbuskirk.me>
2018-10-18 08:45:00 -0700

    Which OS is best?

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Google")
-option_b = os.getenv('OPTION_B', "Azure")
+option_a = os.getenv('OPTION_A', "Apple")
+option_b = os.getenv('OPTION_B', "Windows")
 hostname = socket.gethostname()
 
 app = Flask(__name__)

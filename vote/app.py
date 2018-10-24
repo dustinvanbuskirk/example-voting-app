06de3c32182716c78531434b606b4fc2520ba6ad
Dustin Van Buskirk <dev@vanbuskirk.me>
2018-10-24 09:33:32 -0700

    updating for demo

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Google")
-option_b = os.getenv('OPTION_B', "Azure")
+option_a = os.getenv('OPTION_A', "Bitbucket")
+option_b = os.getenv('OPTION_B', "GitHub")
 hostname = socket.gethostname()
 
 app = Flask(__name__)

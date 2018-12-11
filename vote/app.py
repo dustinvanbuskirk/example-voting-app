1a5714e17a87f9fc5206b480791e1bb7a0913aea
root <cfsalesdemo@gmail.com>
2018-12-11 18:59:56 +0000

    update for denver-or-six-flags

--- vote/app.py
+++ vote/app.py
@@ -5,7 +5,7 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Seattle")
+option_a = os.getenv('OPTION_A', "Denver")
 option_b = os.getenv('OPTION_B', "Six Flags")
 hostname = socket.gethostname()
 

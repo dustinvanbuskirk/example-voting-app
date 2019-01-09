7dc2f9cc0a7e29aea94cb41c79600a19732a56d8
root <cfsalesdemo@gmail.com>
2019-01-09 21:40:11 +0000

    update for denver-or-kings-island

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -32,8 +32,8 @@ def browser():
 
 def test_confirm_vote_title(browser):
     browser.get("http://{}:80".format(vote_endpoint_ip))
-    option_a = "Miami"
-    option_b = "Universal"
+    option_a = "Denver"
+    option_b = "Kings Island"
     assert "{} vs {}!".format(option_a, option_b) in browser.title
 
 def test_confirm_vote_choice_form(browser):

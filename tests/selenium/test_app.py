9973f230ebc3fe48410e178b2d51012716c2edb2
root <cfsalesdemo@gmail.com>
2019-01-10 17:14:25 +0000

    update for new-orleans-or-cedar-point

--- tests/selenium/test_app.py
+++ tests/selenium/test_app.py
@@ -32,8 +32,8 @@ def browser():
 
 def test_confirm_vote_title(browser):
     browser.get("http://{}:80".format(vote_endpoint_ip))
-    option_a = "Portland"
-    option_b = "Kings Island"
+    option_a = "New Orleans"
+    option_b = "Cedar Point"
     assert "{} vs {}!".format(option_a, option_b) in browser.title
 
 def test_confirm_vote_choice_form(browser):

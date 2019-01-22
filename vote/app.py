ee7c99728c99a79e40eff3beed96a2c609c63b4a
root <cfsalesdemo@gmail.com>
2019-01-22 23:16:12 +0000

    update for austin-or-kings-island

--- vote/app.py
+++ vote/app.py
@@ -6,7 +6,7 @@ import random
 import json
 
 option_a = os.getenv('OPTION_A', "Austin")
-option_b = os.getenv('OPTION_B', "Universal")
+option_b = os.getenv('OPTION_B', "Kings Island")
 hostname = socket.gethostname()
 
 app = Flask(__name__)

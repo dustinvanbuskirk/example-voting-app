385fa62ac9832fb3eed31deea5b21d236ac34503
root <cfsalesdemo@gmail.com>
2019-01-24 14:11:21 +0000

    update for miami-or-epcot

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Denver")
-option_b = os.getenv('OPTION_B', "Lego Land")
+option_a = os.getenv('OPTION_A', "Miami")
+option_b = os.getenv('OPTION_B', "Epcot")
 hostname = socket.gethostname()
 
 app = Flask(__name__)

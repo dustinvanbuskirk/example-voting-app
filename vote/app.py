278768d868c872cbcf681a986845cda77dda6556
root <cfsalesdemo@gmail.com>
2019-01-24 18:55:00 +0000

    update for new-orleans-or-cedar-point

--- vote/app.py
+++ vote/app.py
@@ -5,7 +5,7 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Boston")
+option_a = os.getenv('OPTION_A', "New Orleans")
 option_b = os.getenv('OPTION_B', "Cedar Point")
 hostname = socket.gethostname()
 

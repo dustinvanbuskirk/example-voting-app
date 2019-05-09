f04878af4a8afcc44bb2ff3156b7df9770dc4c93
Dustin Van Buskirk <dev@vanbuskirk.me>
2019-05-09 09:55:04 -0500

    updating for devopsdays

--- vote/app.py
+++ vote/app.py
@@ -5,8 +5,8 @@ import socket
 import random
 import json
 
-option_a = os.getenv('OPTION_A', "Docker Swarm")
-option_b = os.getenv('OPTION_B', "Kubernetes")
+option_a = os.getenv('OPTION_A', "AWS")
+option_b = os.getenv('OPTION_B', "Azure")
 hostname = socket.gethostname()
 
 app = Flask(__name__)
